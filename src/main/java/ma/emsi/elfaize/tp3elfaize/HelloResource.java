31b576ab440d46825b293a0ede5591be0991b5aa
Youssef <youssefmae26082000@gmail.com>
2025-11-02 16:05:16 +0100

    Changement du type retour de la méthode hello en Response (statut OK et erreur 500)

--- src/main/java/ma/emsi/elfaize/tp3elfaize/HelloResource.java
+++ src/main/java/ma/emsi/elfaize/tp3elfaize/HelloResource.java
@@ -4,13 +4,18 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Path("/personnes/{nom}")
     @Produces("text/plain")
-    public String hello(@PathParam("nom") String nom) {
-        return "Hello "+ nom;
+    public Response  hello(@PathParam("nom") String nom) {
+        //return Response.ok("Hello, " + nom).build();
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity("Erreur interne : impossible de saluer " + nom)
+                .build();
     }
 }
